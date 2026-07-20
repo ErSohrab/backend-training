bc17ff885f351efc5e1f08bc1df7e0ecec0faf1a
ErSohrab <ersohrab8273@gmail.com>
2026-07-20 10:24:43 +0530

    node and express setup done and adding env file and install dotenv package

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend training API is running",
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
